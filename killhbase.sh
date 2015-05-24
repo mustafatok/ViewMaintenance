fea89f622bb40a91ea920c9e01e5796eb3c836a1
linxj <linxj92@gmail.com>
2015-05-24 11:38:12 +0200

    delete regionserver in kill script

--- killhbase.sh
+++ killhbase.sh
@@ -5,7 +5,7 @@ IN=$(jps)
 read -a ary <<<$IN
 for key in "${!ary[@]}"
 do  
-    if [ "${ary[$key]}" = "HMaster" -o "${ary[$key]}" = "HQuorumPeer" ]
+    if [ "${ary[$key]}" = "HMaster" -o "${ary[$key]}" = "HQuorumPeer" -o "${ary[$key]}" = "HRegionServer" ]
     then
         echo "killing ${ary[$key-1]}"
         kill -9 "${ary[$key-1]}"
