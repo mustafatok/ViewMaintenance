834242fbae39df84bfea61236239c4da8e0489e6
jeff <xiaojielin@actiworks.de>
2015-06-25 09:46:26 +0200

    create conf

--- workspace/BatchStreamingViewMaintenance/src/com/lin/test/SelectCoprocessorImplTest.java
+++ workspace/BatchStreamingViewMaintenance/src/com/lin/test/SelectCoprocessorImplTest.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.Coprocessor;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
@@ -22,53 +23,54 @@ import com.lin.coprocessor.SelectCoprocessorImpl;
 public class SelectCoprocessorImplTest {
 	Configuration conf = null;
 	HTable table = null;
+
 	@Before
-	public void setUp(){
-		// Define a table
-		conf.set("hbase.zookeeper.quorum", "HB");
+	public void setUp() {
 		try {
-			table = new HTable(conf,"bt1");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void test() {
-		Exception exception = null;
-		try {
-			// Get the location of the JAR file containing the coprocessor implementation.
+			// Define a table
+			conf = HBaseConfiguration.create();
+			conf.set("hbase.zookeeper.quorum", "HB");
+			table = new HTable(conf, "bt1");
+
+			// Get the location of the JAR file containing the coprocessor
+			// implementation.
 			FileSystem fs = FileSystem.get(conf);
-			Path path =  new Path(fs.getUri()+Path.SEPARATOR+"BSVM.jar");
-			
+			Path path = new Path(fs.getUri() + Path.SEPARATOR + "BSVM.jar");
+
 			// Define a table descriptor
 			HTableDescriptor htd = new HTableDescriptor("bt1");
 			htd.addFamily(new HColumnDescriptor("colfam1"));
 			// Add the coprocessor definition to the descriptor.
-			htd.setValue("COPROCESSOR$1", path.toString()+
-					"|"+SelectCoprocessorImpl.class.getCanonicalName()+
-					"|"+Coprocessor.PRIORITY_USER);
-			
-			// Instantiate an administrative API to the cluster and add the table.
+			htd.setValue("COPROCESSOR$1", path.toString() + "|"
+					+ SelectCoprocessorImpl.class.getCanonicalName() + "|"
+					+ Coprocessor.PRIORITY_USER);
+
+			// Instantiate an administrative API to the cluster and add the
+			// table.
 			HBaseAdmin admin = new HBaseAdmin(conf);
 			admin.createTable(htd);
-			
+
 			// Verify if the definition has been applied as expected.
 			System.out.println(admin.getTableDescriptor(Bytes.toBytes("bt1")));
-			
+
 		} catch (IOException e) {
-			exception = e;
 			e.printStackTrace();
 		}
-		
-		if(exception != null){
+
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test() {
+		Exception exception = null;
+
+		if (exception != null) {
 			fail("Exception not empty!");
 		}
 	}
-	
+
 	@After
-	public void tearDown(){
+	public void tearDown() {
 		try {
 			table.close();
 		} catch (IOException e) {
