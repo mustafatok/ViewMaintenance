d3fbc67b7db04a0cb3f9c7ba162ba66672bc821d
jeff <xiaojielin@actiworks.de>
2015-06-30 19:21:42 +0200

    try

--- workspace/BatchStreamingViewMaintenance/src/com/lin/coprocessor/SelectCoprocessorImpl.java
+++ workspace/BatchStreamingViewMaintenance/src/com/lin/coprocessor/SelectCoprocessorImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.Coprocessor;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
 import org.apache.hadoop.hbase.client.Scan;
@@ -13,17 +14,15 @@ import org.apache.hadoop.hbase.coprocessor.CoprocessorService;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.protobuf.ResponseConverter;
 import org.apache.hadoop.hbase.regionserver.InternalScanner;
+import org.apache.hadoop.hbase.util.Bytes;
 
-import com.google.protobuf.ByteString;
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
 import com.google.protobuf.Service;
-import com.lin.coprocessor.generated.SelectCoprocessor;
 import com.lin.coprocessor.generated.SelectCoprocessor.Select;
 import com.lin.coprocessor.generated.SelectCoprocessor.SelectRequest;
 import com.lin.coprocessor.generated.SelectCoprocessor.SelectResponse;
 import com.lin.coprocessor.generated.SelectCoprocessor.SelectResponse.Builder;
-import com.lin.coprocessor.generated.SumCoprocessor.SumResponse;
 
 public class SelectCoprocessorImpl extends Select implements
 		Coprocessor, CoprocessorService {
@@ -64,25 +63,34 @@ public class SelectCoprocessorImpl extends Select implements
             List<Cell> results = new ArrayList<Cell>();
             Builder builder = response.newBuilder();
             boolean hasMore = false;
+            byte[] lastRow = null;
+            long count = 0;
             do {
                 hasMore = scanner.next(results);
                 System.out.println("we have " + results.size() + " results here");
                 org.apache.hadoop.hbase.protobuf.generated.ClientProtos.Result oneResult = null;
                 for (Cell cell : results) {
-                	System.out.println("getFamilyArray:"+cell.getFamilyArray());
-                	System.out.println("getFamily:"+cell.getFamily());
-                	System.out.println("ByteString.copyFrom:"+ByteString.copyFrom(cell.getFamilyArray()));
-                	System.out.println("getValueArray:"+cell.getValueArray());
-                	System.out.println("getValue:"+cell.getValue());
-                	System.out.println("ByteString.copyFrom:"+ByteString.copyFrom(cell.getValueArray()));
-                	//result = Long.parseLong(new String(cell.getValue()));
-                    org.apache.hadoop.hbase.protobuf.generated.CellProtos.Cell cellMessage = org.apache.hadoop.hbase.protobuf.generated.CellProtos.Cell.newBuilder()
-                    		.setFamily(ByteString.copyFrom(cell.getFamilyArray()))
-                    		.setRow(ByteString.copyFrom(cell.getRowArray()))
-                    		.setValue(ByteString.copyFrom(cell.getValueArray())).build(); 
-                    oneResult = oneResult.newBuilder().addCell(cellMessage).build();
+                	byte[] currentRow = CellUtil.cloneRow(cell);
+                    if (lastRow == null || !Bytes.equals(lastRow, currentRow)) {
+                      lastRow = currentRow;
+                      count++;
+                    }
+//                	System.out.println("getFamilyArray:"+cell.getFamilyArray());
+//                	System.out.println("getFamily:"+cell.getFamily());
+//                	System.out.println("ByteString.copyFrom:"+ByteString.copyFrom(cell.getFamilyArray()));
+//                	System.out.println("getValueArray:"+cell.getValueArray());
+//                	System.out.println("getValue:"+cell.getValue());
+//                	System.out.println("ByteString.copyFrom:"+ByteString.copyFrom(cell.getValueArray()));
+//                	//result = Long.parseLong(new String(cell.getValue()));
+//                    org.apache.hadoop.hbase.protobuf.generated.CellProtos.Cell cellMessage = org.apache.hadoop.hbase.protobuf.generated.CellProtos.Cell.newBuilder()
+//                    		.setFamily(ByteString.copyFrom(cell.getFamilyArray()))
+//                    		.setRow(ByteString.copyFrom(cell.getRowArray()))
+//                    		.setValue(ByteString.copyFrom(cell.getValueArray())).build(); 
+//                    oneResult = oneResult.newBuilder().addCell(cellMessage).build();
                 }
                 results.clear();
+                
+                System.out.println("the count is " + count);
                 
                 builder.addResultRows(oneResult);
             } while (hasMore);
